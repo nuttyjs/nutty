19c65d267b1c8edf61e26be3474aec4d7d5a3692
jmjuanes <josemijuanes@gmail.com>
2017-01-24 23:55:09 +0100

    index.js: big rewrite

--- index.js
+++ index.js
@@ -1,94 +1,57 @@
-//Import dependencies
-var json = require('ujson');
-
 //Import libs
-var Args = require('./lib/args.js');
-var Commands = require('./lib/commands.js');
-var Display = require('./lib/display.js');
-var Help = require('./lib/help.js');
-var Options = require('./lib/options.js');
+var args_parser = require('./lib/args.js');
+var commands = require('./lib/commands.js');
+var display = require('./lib/display.js');
+var help = require('./lib/help.js');
+var options = require('./lib/options.js');
 
-//Import config
-var config = require('./config.js');
+//nutty object
+var nutty = {};
 
-//Nutty object
-var Nutty = { display: Display };
+//Middlewares list
+nutty._middlewares = [];
 
-//Nutty storage object
-Nutty.storage =
+//List of commands
+nutty._commands = [];
+
+//Settings
+nutty._settings = { name: '', description: '', version: '' };
+
+//Add a setting value
+nutty.set = function(key, value)
 {
-  //Get from storage
-  get: function(key)
-  {
-    //Get from the storage
-    return config.storage.content[key];
-  },
+  //Check for undefined key or value
+  if(typeof key !== 'string' || typeof value === 'undefined'){ return; }
 
-  //set on storage
-  set: function(key, value)
-  {
-    //Set on the local storage
-    config.storage.content[key] = value;
-
-    //Save to a file
-    json.writeSync(config.storage.path, config.storage.content, { encoding: 'utf8', jsonSpace: '  ' });
-  },
-
-  //Get the storage path
-  path: function(){ return config.storage.path; }
+  //Change the setting value
+  nutty._settings[key] = value;
 };
 
-//Set the CLI name
-Nutty.name = function(value)
+//get a setting value
+nutty.get = function(key)
 {
   //Check for undefined
-  if(typeof value === 'undefined'){ return config.name; }
+  if(typeof key !== 'string'){ return; }
 
-  //Set the name
-  config.name = value;
-
-  //Initialize the storage
-  config.storage.init();
+  //Return the settings value
+  return nutty._settings[key];
 };
 
-//Set the CLI description
-Nutty.description = function(value)
+//Use a middleware
+nutty.use = function(fn)
 {
-  //Check for undefined
-  if(typeof value === 'undefined'){ return config.description; }
+  //Check for function
+  if(typeof fn !== 'function'){ return; }
 
-  //Set the description
-  config.description = value;
-};
+  //Save as a middleware
+  nutty._middlewares.push(fn);
 
-//Set the CLI version
-Nutty.version = function(value)
-{
-  //Check for undefined
-  if(typeof value === 'undefined'){ return config.version; }
-
-  //Set the version
-  config.version = value;
-};
-
-//Set the nutty values
-Nutty.set = function(opt)
-{
-  //Check the opt data
-  if(typeof opt !== 'object'){ var opt = {}; }
-
-  //Check the CLI name
-  if(typeof opt.name === 'string'){ Nutty.name(opt.name); }
-
-  //Check the CLI description
-  if(typeof opt.description === 'string'){ Nutty.description(opt.description); }
-
-  //Check the CLI version
-  if(typeof opt.version === 'string'){ Nutty.version(opt.version); }
+  //Continue
+  return;
 };
 
 //Add a new command
-Nutty.add = function(list)
+nutty.add = function(list)
 {
   //Check the list of commands
   if(typeof list !== 'object'){ return; }
@@ -117,70 +80,28 @@ Nutty.add = function(list)
 };
 
 //Run the cli
-Nutty.run = function(middleware)
+nutty.run = function()
 {
-  //Check the middleware
-  if(typeof middleware !== 'function'){ var middleware = function(c,a,o,n){ return n(); }; }
+  //Parse the arguments
+  var args = args_parser();
 
-  //Input cases
-  // myapp   -> display help
-  // myapp help  -> display help
-  // myapp <cmd> <args>  -> Run the command
-  // myapp <args>  -> Run the default command if exists, else display error
-
-  //Get the arguments
-  var args = Args();
-
-  //Get the command
-	var command = (args.arguments.length === 0) ? 'help' : args.arguments[0];
-
-  //Check for help
-  if(command === 'help'){ return Help(args.arguments); }
-
-  //Check if has default command
-  var has_default = Commands.default.has();
-
-  //Check if command exists
-  var has_command = Commands.has(command);
-
-  //Find the command to execute
-  if(has_default === true || has_command === true)
+  //Middlewares recursive caller
+  var middlewares_recursive = function(index)
   {
-    //Check if the command exists
-    if(has_command === true)
+    //Check the index value
+    if(index >= nutty._middlewares.length){ return help(args.arguments, opt.options); }
+
+    //Call the middleware
+    nutty._middlewares[index](args.arguments, args.options, function()
     {
-      //Remove the first argument
-      args.arguments.shift();
-    }
-    else
-    {
-      //Get the default command
-      command = Commands.default.get();
-    }
-
-    //Get the command object
-    var obj = config.commands[command];
-
-    //Parse the options
-    var parsed = Options(args.options, obj.options);
-
-    //Check for error
-    if(parsed.error === true){ return ; }
-
-    //Run the middleware
-    return middleware(command, args.arguments, parsed.options, function()
-    {
-      //Run the command function
-      obj.callback(args.arguments, parsed.options);
+      //Next middleware on the list
+      return middlewares_recursive(index + 1);
     });
-  }
+  };
 
-  //Show error in console
-  Display.error('Unknow command "' + command + '"');
-
-  //Exit
-  return;
+  //Read all the middlewares recursive
+  return middlewares_recursive(0);
 };
 
 //Exports to node
-module.exports = Nutty;
+module.exports = nutty;
