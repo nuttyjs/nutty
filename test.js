24f35ace3ae4e68b19fbb52e0b68cb5eea90ba29
jmjuanes <josemijuanes@gmail.com>
2017-01-25 11:28:28 +0100

    test.js: updated test

--- test.js
+++ test.js
@@ -1,40 +1,26 @@
 //Import nutty
 var nutty = require('./index.js');
 
-//Get hello function
-function GetHello(args, options)
+//Set the CLI name
+nutty.set('name', 'myapp');
+
+//Set the CLI description
+nutty.set('description', 'My test app');
+
+//Set the CLI version
+nutty.set('version', '1.0.0');
+
+//Use a middleware
+nutty.use(function(args, opt, next)
 {
   //Get the name
   var name = args[0];
 
   //Get the hello word
-  var hello = (options.idiom === 'english') ? 'Hello' : 'Hola';
+  var hello = (opt.idiom === 'english') ? 'Hello' : 'Hola';
 
-  //Display
+  //Display in console
   console.log('>>>>>>> ' + hello + ' ' + name + '!');
-
-  //Display done message
-  nutty.display.done('');
-}
-
-//Set the CLI name
-nutty.name('myapp');
-
-//Set the CLI description
-nutty.description('My test app');
-
-//Set the CLI version
-nutty.version('1.0.0');
-
-//Add a basic command
-nutty.add({
-  command: 'hello',
-  description: 'Get hello with your name',
-  usage: 'myapp hello [your_name][options]',
-  callback: GetHello,
-  options: [
-    { name: 'idiom', description: 'Set the idiom. Available idioms: english|spanish', default: 'english', mandatory: true }
-  ]
 });
 
 //Run the CLI
